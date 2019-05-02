6b998f38d19165b4bcc7915d7542c815666daa2f
Xett <3954937+Xett@users.noreply.github.com>
2019-05-03 08:41:12 +1000

    added os check, and if not windows it makes a symbolic link in shell

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 SCRIPT=$(readlink -f "$0")
 PROJECT_DIRECTORY=$(dirname "$SCRIPT")
 cp -rl hooks .git
-#ln -s $PROJECT_DIRECTORY"/www/public/wp-content/themes" "themes"
+if [[ "$OSTYPE" == "win32"]]; then
+   # ...
+else
+   ln -s $PROJECT_DIRECTORY"/www/public/wp-content/themes" "themes"
 
 # Pause at end
 read -p "Press enter to continue"
