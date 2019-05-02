d8dfa8f9950ca7acd302ab6ec075e7cd0fd55b85
Xett <3954937+Xett@users.noreply.github.com>
2019-05-03 08:33:54 +1000

    update install to link to themes

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT=$(readlink -f "$0")
 PROJECT_DIRECTORY=$(dirname "$SCRIPT")
 cp -rl hooks .git
+ln -s $PROJECT_DIRECTORY"/www/public/wp-content/themes" "themes"
 
 # Pause at end
 read -p "Press enter to continue"
