26e77c9831c54e19d774381a20ccf17fd18c276c
Xett <3954937+Xett@users.noreply.github.com>
2019-05-05 03:19:47 +1000

    Move set_update_commands function to start.sh

--- scripts/update_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set_update_commands()
-{
-   COMMANDS[2]="sudo mysql -u'root' -p'root' -e \"drop database $MYSQL_DATABASE_NAME\"; echo \"Clearing MYSQL Database\"; "
-   COMMANDS[3]="sudo mysql -u'root' -p'root' -e \"create database $MYSQL_DATABASE_NAME\"; echo \"Creating $MYSQL_DATABASE_NAME Database\"; "
-   COMMANDS[4]="sudo mysql -u'root' -p'root' -e \"use $MYSQL_DATABASE_NAME\"; echo \"Selecting Database $MYSQL_DATABASE_NAME\"; "
-   COMMANDS[5]="sudo mysql -u'root' -p'root' $MYSQL_DATABASE_NAME < $MYSQL_DUMP; echo \"Updating MYSQL Database from MYSQL Dump File\"; "
-}
--- start.sh
+++ start.sh
@@ -17,8 +17,13 @@ COMMANDS[20]="sudo service apache2 start; echo \"Starting Apache Web Server\"; "
 . $PROJECT_DIRECTORY"/scripts/update_database.sh"
 # Import create_webadmin function
 . $PROJECT_DIRECTORY"/scripts/create_mysql_webadmin.sh"
-# Import set_update_commands function
-. $PROJECT_DIRECTORY"/scripts/update_commands.sh"
+set_update_commands()
+{
+   COMMANDS[2]="sudo mysql -u'root' -p'root' -e \"drop database $MYSQL_DATABASE_NAME\"; echo \"Clearing MYSQL Database\"; "
+   COMMANDS[3]="sudo mysql -u'root' -p'root' -e \"create database $MYSQL_DATABASE_NAME\"; echo \"Creating $MYSQL_DATABASE_NAME Database\"; "
+   COMMANDS[4]="sudo mysql -u'root' -p'root' -e \"use $MYSQL_DATABASE_NAME\"; echo \"Selecting Database $MYSQL_DATABASE_NAME\"; "
+   COMMANDS[5]="sudo mysql -u'root' -p'root' $MYSQL_DATABASE_NAME < $MYSQL_DUMP; echo \"Updating MYSQL Database from MYSQL Dump File\"; "
+}
 # Run commands function
 run()
 {
